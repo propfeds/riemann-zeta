51e21be2c7933ac854acd7a256f418313090ff72
Minh <propfeds@gmail.com>
2024-12-26 23:39:40 +0900

    Flipped sign of bhdt

--- theory.js
+++ theory.js
@@ -1332,17 +1332,17 @@ var tick = (elapsedTime, multiplier) =>
             if(blackhole && t >= 14 && !dTerm.isZero)
             {
                 let dNewt = (tmpZ[2] - zResult[2]) * derivRes;
-                let bhdt = Math.min(Math.max(-1, zResult[2] / dNewt), 1);
+                let bhdt = Math.max(-1, -zResult[2] / dNewt);
 
-                if(searchingRewind && bhdt < 0)
+                if(searchingRewind && bhdt > 0)
                 {
                     t_dot = t_resolution;
                     t += t_dot * elapsedTime;
                 }
                 else
                 {
-                    t_dot = -bhdt / elapsedTime;
-                    t -= bhdt;
+                    t_dot = bhdt / elapsedTime;
+                    t += bhdt;
                     searchingRewind = false;
                     if(Math.abs(bhdt) < 1e-9)
                     {
@@ -1642,7 +1642,7 @@ var resetStage = () =>
     // This points lastZero to a non-zero, necessary sacrifice.
     lastZero = 0;
     foundZero = false;
-    
+
     disableBlackhole();
     // Prevent lastZero from opening
     zResult[2] = NaN;
